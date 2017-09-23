a2af7af6ad9075f767825e1e2e91e39df4c1a20c
Ángel Rodríguez <angel_r_m2001@hotmail.com>
2017-09-23 00:19:39 -0500

    te comente los errores, ya no se que mas intentar para corregirlos

--- aprioriInformation.m
+++ aprioriInformation.m
@@ -9,32 +9,84 @@ import recognition.*
 prostateImg = imread('Imagenes/an-u3-15-03.tif');
 imshow(prostateImg);
 prostateImgCut = imcrop(prostateImg,[120 110 420 350]);
+figure();
 imshow(prostateImgCut);
 
 %%Aplying Gausian filter of thirteenth order
 pascal13=[1 12 66 220 495 792 924 792 495 220 66 12 1];
 matrizGausiana13=pascal13'*pascal13/(sum(pascal13)^2);
 prostateImgCutBlurry=uint8(conv2(prostateImgCut,matrizGausiana13,'same'));
+figure();
 imshow(prostateImgCutBlurry);
 
 %%Getting grayscale average for prostate
 hFH1 = imfreehand();
 xypositionP = getPosition(hFH1);
-graylevelAverageProstate = sum(xypositionP)/size(xypositionP);
+[i,z] = size(xypositionP);
+sumVals = double(0);
+matValuesP = [i,z];
+for x=1:i
+    for y=1:z
+        sumVals = sumVals + double(prostateImgCutBlurry(int16(xypositionP(x)),int16(xypositionP(x,y))));
+        matValuesP(x,y) = double(prostateImgCutBlurry(int16(xypositionP(x)),int16(xypositionP(x,y))));
+    end
+end
+graylevelAverageProstate = sumVals/(i*z);
+close;
 
 %Getting grayscale average for halo
+figure();
+imshow(prostateImgCutBlurry);
 hFH2 = imfreehand();
 xypositionH = getPosition(hFH2);
-graylevelAverageHalo = sum(xypositionH)/size(xypositionH);
+[i,z] = size(xypositionH);
+matValuesH = [i,z];
+sumVals = double(0);
+for x=1:i
+    for y=1:z
+        if int16(xypositionH(x)) > 351
+            sumVals = sumVals + double(prostateImgCutBlurry(351,int16(xypositionH(x,y))));
+            matValuesH(x,y) = double(prostateImgCutBlurry(351,int16(xypositionH(x,y))));
+        else
+             sumVals = sumVals + double(prostateImgCutBlurry(int16(xypositionH(x)),int16(xypositionH(x,y))));
+             matValuesH(x,y) = double(prostateImgCutBlurry(int16(xypositionH(x)),int16(xypositionH(x,y))));
+        end
+    end
+end
+graylevelAverageHalo = sumVals/(i*z);
+close;
 
 %Getting region of background
+figure();
+imshow(prostateImgCutBlurry);
 hFH3 = imfreehand();
 xypositionB = getPosition(hFH3);
+[i,z] = size(xypositionB);
+matValuesB = [i,z];
+for x=1:i
+    for y=1:z
+        if int16(xypositionB(x)) > 351
+            matValuesB(x,y) = double(prostateImgCutBlurry(351,int16(xypositionB(x,y))));
+        else
+             matValuesB(x,y) = double(prostateImgCutBlurry(int16(xypositionB(x)),int16(xypositionB(x,y))));
+        end
+    end
+end
+close;
 
-probP = (size(prostateImgCutBlurry) - size(xypositionP))/ size(prostateImgCutBlurry);
-probH = (size(prostateImgCutBlurry) - size(xypositionH))/ size(prostateImgCutBlurry);
+[nR,nC]=size(prostateImgCutBlurry);
+[nRP,nCP]=size(matValuesP);
+[nRH,nCH]=size(matValuesH);
+
+%%NO LOGRO DESCIFRAR EL ERROR, ENTIENDO PORQUE SUCEDE PERO NO SE COMO
+%%CORREGIRLO
+%%EL PROBLEMA ES QUE LA MATRIZ DE VALORES DE HALO ES MAS GRANDE QUE LA
+%%MATRIZ TOTAL DE LA IMAGEN, EL ERROR VIENE DE IMFREEHAND PERO NO ENTIENDO
+%%PORQUE SUCEDE. RIP
+probP = (nR*nC - nRP*nCP)/ nR*nC;
+probH = (nR*nC - nRH*nCH)/ nR*nC;%%ESTA PROBABILIDAD DA NEGATIVA
 probB = 1 - probP - probH;
 
-covP = cov(xypositionP);
-covH = cov(xypositionH);
-covB = cov(xypositionB);
+covP = cov(matValuesP);
+covH = cov(matValuesH);
+covB = cov(matValuesB);
--- recognition.m
+++ recognition.m
@@ -1,35 +1,56 @@
-function cuttedImg = Cut( image )
-    cuttedImg = imcrop(image,[120 110 420 350]);
-end
+img = imread('Imagenes/an-u3-15-03.tif');
+cuttedImg = imcrop(img,[120 110 420 350]);
 
-function blur = Blur( grayImage )
+pascal13=[1 12 66 220 495 792 924 792 495 220 66 12 1];
+matrizGausiana13=pascal13'*pascal13/(sum(pascal13)^2);
+blur=uint8(conv2(cuttedImg,matrizGausiana13,'same'));
 
-    pascal13=[1 12 66 220 495 792 924 792 495 220 66 12 1];
-    matrizGausiana13=pascal13'*pascal13/(sum(pascal13)^2);
-    blur=uint8(conv2(grayImage,matrizGausiana13,'same'));
+pixelsH=double(0);
+pixelsP=double(0);
+pixelsB=double(0);
 
-end
-
-function [pixelsP, pixelsH, pixelsB] = countPixels( image )
-    [nRows,nCols] = size(image);
-    for x=1:nRows
-        for y=1:nCols
-            if image(x,y)>=graylevelAverageHalo
-                pixelsH = pixelsH + 1;
-            elseif (image(x,y)<graylevelAverageHalo) && (image(x,y)>=graylevelAverageProstate)
-                pixelsP = pixelsP + 1;
-            else
-                pixelsB = pixelsB + 1;
-            end
+[nRows,nCols] = size(blur);
+for x=1:nRows
+    for y=1:nCols
+        if (blur(x,y)>=graylevelAverageProstate-5.0) && (blur(x,y)<=graylevelAverageProstate+5.0)
+            pixelsP = pixelsP + double(1);
+            blur(x,y)
+        elseif (blur(x,y)<=graylevelAverageProstate+5.0) && (blur(x,y)>=graylevelAverageHalo-5.0)
+            pixelsH = pixelsH + double(1);
+        else
+            pixelsB = pixelsB + double(1);
         end
     end
 end
 
+
 %Tengo una duda con la formula del discriminante bayesiano. Segun como la
-%veo, si multiplican escalares por la inversa de una matriz. ¿Eso no
+%veo, si multiplican escalares por la inversa de una matriz. ï¿½Eso no
 %regresa una matriz?
 
 %%Falta implementar la funcion que regrese las funciones de bayes.
 
+[nRows, nColumns] = size(blur);
 
+difP = 0;
+difH = 0;
+difPB = 0;
+
+discriminants=[3];
+
+meanP = pixelsP / nRows*nColumns;
+meanH = pixelsH / nRows*nColumns;
+meanB = pixelsB / nRows*nColumns;
+
+for x=1:nRows
+    for y=1:nColumns
+        difP = blur(x,y) - meanP;
+        difH = blur(x,y) - meanH;
+        difB = blur(x,y) - meanB;
+
+        discriminants(1) = -1/2 * difP' * inv(covP) * difP -1/2 * log(det(covP)) + log(probP);
+        discriminants(2) = -1/2 * difH' * inv(covH) * difH -1/2 * log(det(covH)) + log(probH);
+        discriminants(3) = -1/2 * difB' * inv(covB) * difP -1/2 * log(det(covB)) + log(probB);
+    end
+end
 
