eb8aa985c296f8e2630fb2f9d181eddf2778567f
Ángel Rodríguez <angel_r_m2001@hotmail.com>
2017-09-30 21:59:34 -0500

    al dia

--- recognition.m
+++ recognition.m
@@ -1,13 +1,19 @@
-img = imread('Imagenes/an-u3-15-03.tif');
+img = imread('Imagenes/U3-14-04.BMP');
 cuttedImg = imcrop(img,[120 110 420 350]);
 
 pascal13=[1 12 66 220 495 792 924 792 495 220 66 12 1];
 matrizGausiana13=pascal13'*pascal13/(sum(pascal13)^2);
 blur=uint8(conv2(cuttedImg,matrizGausiana13,'same'));
+figure();
+imagesc(blur);
+colormap gray;
 
 pixelsH=double(0);
 pixelsP=double(0);
 pixelsB=double(0);
+sumValsP=double(0);
+sumValsH=double(0);
+sumValsB=double(0);
 xP=double(0);
 yP=double(0);
 xH=double(0);
@@ -17,43 +23,45 @@ yB=double(0);
 [nRows,nCols] = size(blur);
 for x=1:nRows
     for y=1:nCols
-        if (blur(x,y)>graylevelAverageProstate(1))
-            pixelsH = pixelsH + double(blur(x,y));
+        if (blur(x,y)>=graylevelAverageHalo(1)) 
+            pixelsH = pixelsH + double(1);
+            sumValsH = sumValsH + double(blur(x,y));
             xH = xH + x;
             yH = yH + y;
-        elseif (blur(x,y)>graylevelAverageBack(1)) 
-            pixelsP = pixelsP + double(blur(x,y));
+        elseif (blur(x,y)>=graylevelAverageProstate(1)) 
+            pixelsP = pixelsP + double(1);
+            sumValsP = sumValsP + double(blur(x,y));
             xP = xP + x;
             yP = yP + y;
         else
-            pixelsB = pixelsB + double(blur(x,y));
+            pixelsB = pixelsB + double(1);
+            sumValsB = sumValsB + double(blur(x,y));
             xB = xB + x;
             yB = yB + y;
         end
     end
 end
     
-[nRows, nColumns] = size(blur);
 discriminants=(3);
 X_t = (double(3));
 
-meanP(1) = pixelsP / (nRows*nColumns);
-meanP(2) = xP / (nRows);
-meanP(3) = yP / (nColumns);
+meanP(1) = sumValsP / pixelsP;
+meanP(2) = xP / pixelsP;
+meanP(3) = yP / pixelsP;
 
-meanH(1) = pixelsH / (nRows*nColumns);
-meanH(2) = xH / (nRows);
-meanH(3) = yH / (nColumns);
+meanH(1) = sumValsH / pixelsH;
+meanH(2) = xH / pixelsH;
+meanH(3) = yH / pixelsH;
 
-meanB(1) = pixelsB / (nRows*nColumns);
-meanB(2) = xB / (nRows);
-meanB(3) = yB / (nColumns);
+meanB(1) = sumValsB / pixelsB;
+meanB(2) = xB / pixelsB;
+meanB(3) = yB / pixelsB;
 
 
 filteredImg = blur;
 
 for x=1:nRows
-    for y=1:nColumns
+    for y=1:nCols
         X_t(1) = blur(x,y);
         X_t(2) = x;
         X_t(3) = y;
@@ -62,12 +70,13 @@ for x=1:nRows
         difH = double(X_t) - meanH;
         difB = double(X_t) - meanB;
 
-        discriminants(1) = -1/2 * difP' * inv(covP) * difP -1/2 * log(det(covP)) + log(probP)
-        discriminants(2) = -1/2 * difH' * inv(covH) * difH -1/2 * log(det(covH)) + log(probH)
-        discriminants(3) = -1/2 * difB' * inv(covB) * difB -1/2 * log(det(covB)) + log(probB) 
+        discriminants(1) = -1/2 * difP * inv(covP) * difP' -1/2 * log(det(covP)) + log(probP);
+        discriminants(2) = -1/2 * difH * inv(covH) * difH' -1/2 * log(det(covH)) + log(probH);
+        discriminants(3) = -1/2 * difB * inv(covB) * difB' -1/2 * log(det(covB)) + log(probB);
+        %discriminants
         
         if (discriminants(1) > discriminants(2)) && (discriminants(1) > discriminants(3))
-            filteredImg(x,y) = 250;
+            filteredImg(x,y) = 255;
         elseif (discriminants(2) > discriminants(1)) && (discriminants(2) > discriminants(3))
             filteredImg(x,y) = 128;
         else
@@ -76,6 +85,7 @@ for x=1:nRows
     end
 end
 
+figure();
 imagesc(filteredImg);
 colormap gray;
 
